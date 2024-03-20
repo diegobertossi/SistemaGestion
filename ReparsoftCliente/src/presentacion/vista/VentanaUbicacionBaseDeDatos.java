4284e682b8f4dc922d710516a219360a5b97d1e3
Diego <Diego@DESKTOP-PTCAS9P>
2024-03-20 16:08:20 -0300

    logo inicio

--- ReparsoftCliente/src/presentacion/vista/VentanaUbicacionBaseDeDatos.java
+++ ReparsoftCliente/src/presentacion/vista/VentanaUbicacionBaseDeDatos.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
 import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class VentanaUbicacionBaseDeDatos extends JFrame 
 {
@@ -32,45 +33,49 @@ public class VentanaUbicacionBaseDeDatos extends JFrame
 		this.setLocationRelativeTo(null);
 
 		contentPane = new JPanel();
+		contentPane.setOpaque(false);
 		contentPane.setBackground(SystemColor.inactiveCaption);
 		contentPane.setBorder(new LineBorder(new Color(0, 128, 128)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 255), 2));
-		panel.setOpaque(false);
+		panel.setBackground(new Color(176, 196, 222));
+		panel.setBorder(new LineBorder(new Color(0, 128, 128)));
 		panel.setBounds(53, 256, 339, 104);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 				
 		btnBuenosAires = new JButton("<html><center>BUENOS AIRES</html>");
-		btnBuenosAires.setBackground(new Color(0, 139, 139));
+		btnBuenosAires.setBackground(new Color(176, 196, 222));
 		btnBuenosAires.setForeground(new Color(255, 255, 255));
 		btnBuenosAires.setBounds(46, 43, 100, 43);
 		panel.add(btnBuenosAires);
-		btnBuenosAires.setFont(new Font("Roboto", Font.BOLD, 14));
+		btnBuenosAires.setFont(new Font("Cambria", Font.BOLD, 14));
 		
 			
 			btnBariloche = new JButton("<html><center>BARILOCHE</html>");
-			btnBariloche.setBackground(new Color(0, 139, 139));
+			btnBariloche.setBackground(new Color(176, 196, 222));
 			btnBariloche.setForeground(new Color(255, 255, 255));
 			btnBariloche.setBounds(192, 40, 100, 50);
 			panel.add(btnBariloche);
-			btnBariloche.setFont(new Font("Roboto", Font.BOLD, 14));
+			btnBariloche.setFont(new Font("Cambria", Font.BOLD, 14));
 			
 			JLabel lblNewLabel_1 = new JLabel("ELIJA LA UBICACION DEL SISTEMA :");
+			lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewLabel_1.setForeground(new Color(255, 255, 255));
-			lblNewLabel_1.setBounds(54, 4, 231, 33);
+			lblNewLabel_1.setBounds(10, 4, 319, 33);
 			panel.add(lblNewLabel_1);
-			lblNewLabel_1.setFont(new Font("Roboto", Font.BOLD, 14));
+			lblNewLabel_1.setFont(new Font("Cambria", Font.BOLD, 18));
 			
 			lblreparsoft = new JLabel("");
-			lblreparsoft.setIcon(new ImageIcon("F:\\Users\\Diego\\git\\SistemaGestion\\ReparsoftCliente\\img\\REPARSOFT logo.png"));
+			lblreparsoft.setIcon(new ImageIcon("F:\\Users\\Diego\\git\\SistemaGestion\\ReparsoftCliente\\img\\REPARSOFT logo Inicio.png"));
+			lblreparsoft.setVerticalAlignment(SwingConstants.TOP);
+			lblreparsoft.setHorizontalAlignment(SwingConstants.CENTER);
 			lblreparsoft.setForeground(new Color(105, 105, 105));
-			lblreparsoft.setFont(new Font("Magneto", Font.BOLD, 32));
-			lblreparsoft.setBounds(3, 3, 445, 385);
+			lblreparsoft.setFont(new Font("Tahoma", Font.BOLD, 32));
+			lblreparsoft.setBounds(1, -9, 445, 400);
 			contentPane.add(lblreparsoft);
 
 		
