a7c138e2b43355f37b2cb5df9b91af0f3e60f2a3
diegobertossi <diego.bertossi@gmail.com>
2025-04-05 12:43:29 -0300

    combobox

--- ReparsoftCliente/src/VistaPropias/AutoCompletarComboBox.java
+++ ReparsoftCliente/src/VistaPropias/AutoCompletarComboBox.java
@@ -1,104 +1,69 @@
 package VistaPropias;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import javax.swing.ComboBoxEditor;
-import javax.swing.ComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.JTextComponent;
-import javax.swing.text.PlainDocument;
+
+import javax.swing.*;
+import javax.swing.text.*;
 
 /**
- * Clase para habilitar autocompletado en JComboBox, permitiendo cualquier entrada.
+ * Clase para habilitar autocompletado en JComboBox, permitiendo entrada libre
+ * si es editable.
  */
-public final class AutoCompletarComboBox extends PlainDocument {
-    JComboBox comboBox;
-    ComboBoxModel model;
-    JTextComponent editor;
-    boolean selecting = false;
-    boolean hidePopupOnFocusLoss;
-    boolean hitBackspace = false;
-    boolean hitBackspaceOnSelection;
+public class AutoCompletarComboBox extends PlainDocument {
 
-    // Listeners para editor (deben ser inicializados)
-    private KeyAdapter editorKeyListener;
-    private FocusAdapter editorFocusListener;
+    private final JComboBox comboBox;
+    private ComboBoxModel model;
+    private JTextComponent editor;
+    private boolean selecting = false;
+    private boolean hidePopupOnFocusLoss;
+    private boolean hitBackspace = false;
+    private boolean hitBackspaceOnSelection;
+    private final boolean mostrarMensaje;
+    private final boolean editable;
+    private boolean mensajeMostrado = false;
 
-    public AutoCompletarComboBox(final JComboBox comboBox) {
+    public AutoCompletarComboBox(final JComboBox comboBox, boolean editable, boolean mostrarMensaje) {
         this.comboBox = comboBox;
-        model = comboBox.getModel();
+        this.model = comboBox.getModel();
+        this.editable = editable;
+        this.mostrarMensaje = mostrarMensaje;
 
-        // Configurar acción del ComboBox
-        comboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!selecting) highlightCompletedText(0);
-            }
-        });
+        comboBox.setEditable(true);
 
-        // Configurar cambios en propiedades del ComboBox
-        comboBox.addPropertyChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent e) {
-                if (e.getPropertyName().equals("editor")) configureEditor((ComboBoxEditor) e.getNewValue());
-                if (e.getPropertyName().equals("model")) model = (ComboBoxModel) e.getNewValue();
-            }
-        });
-
-        // Inicializar KeyListener
-        editorKeyListener = new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (comboBox.isDisplayable()) comboBox.setPopupVisible(true);
-                hitBackspace = false;
-                switch (e.getKeyCode()) {
-                    case KeyEvent.VK_BACK_SPACE:
-                        hitBackspace = true;
-                        hitBackspaceOnSelection = editor.getSelectionStart() != editor.getSelectionEnd();
-                        break;
-                    case KeyEvent.VK_DELETE:
-                        e.consume();
-                        //comboBox.getToolkit().beep();
-                        break;
-                }
-            }
-        };
-
-        // Inicializar FocusListener
-        hidePopupOnFocusLoss = System.getProperty("java.version").startsWith("1.5");
-        editorFocusListener = new FocusAdapter() {
-            @Override
-            public void focusGained(FocusEvent e) {
+        comboBox.addActionListener(e -> {
+            if (!selecting) {
                 highlightCompletedText(0);
             }
+        });
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (hidePopupOnFocusLoss) comboBox.setPopupVisible(false);
+        comboBox.addPropertyChangeListener(e -> {
+            if (e.getPropertyName().equals("editor")) {
+                configureEditor((ComboBoxEditor) e.getNewValue());
             }
-        };
+            if (e.getPropertyName().equals("model")) {
+                model = (ComboBoxModel) e.getNewValue();
+            }
+        });
 
-        // Configurar el editor
+        hidePopupOnFocusLoss = false;
+
         configureEditor(comboBox.getEditor());
+
         Object selected = comboBox.getSelectedItem();
-        if (selected != null) setText(selected.toString());
+        if (selected != null) {
+            setText(selected.toString());
+        }
+
         highlightCompletedText(0);
     }
 
-    public static void enable(JComboBox comboBox) {
-        comboBox.setEditable(true);
-        new AutoCompletarComboBox(comboBox);
+    public static void enable(JComboBox comboBox, boolean editable, boolean mostrarMensaje) {
+        new AutoCompletarComboBox(comboBox, editable, mostrarMensaje);
     }
 
-    void configureEditor(ComboBoxEditor newEditor) {
+    private void configureEditor(ComboBoxEditor newEditor) {
         if (editor != null) {
             editor.removeKeyListener(editorKeyListener);
             editor.removeFocusListener(editorFocusListener);
@@ -112,6 +77,52 @@ public final class AutoCompletarComboBox extends PlainDocument {
         }
     }
 
+    private final KeyListener editorKeyListener = new KeyAdapter() {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (comboBox.isDisplayable()) comboBox.setPopupVisible(true);
+            hitBackspace = false;
+
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_BACK_SPACE:
+                    hitBackspace = true;
+                    hitBackspaceOnSelection = editor.getSelectionStart() != editor.getSelectionEnd();
+                    break;
+                case KeyEvent.VK_ENTER:
+                case KeyEvent.VK_TAB:
+                    verificarItemNoEncontrado();
+                    break;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            String text = editor.getText();
+            Object item = lookupItem(text);
+
+            if (item == null) {
+                comboBox.setPopupVisible(false);
+            } else {
+                comboBox.setPopupVisible(true);
+            }
+        }
+    };
+
+    private final FocusListener editorFocusListener = new FocusAdapter() {
+        @Override
+        public void focusGained(FocusEvent e) {
+            highlightCompletedText(0);
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            if (hidePopupOnFocusLoss) {
+                comboBox.setPopupVisible(false);
+            }
+            verificarItemNoEncontrado();
+        }
+    };
+
     @Override
     public void remove(int offs, int len) throws BadLocationException {
         if (selecting) return;
@@ -120,19 +131,17 @@ public final class AutoCompletarComboBox extends PlainDocument {
 
     @Override
     public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-        if (selecting) return;
+        if (selecting || str == null) return;
         super.insertString(offs, str, a);
 
-        String currentText = getText(0, getLength());
-        Object item = lookupItem(currentText);
+        String content = getText(0, getLength());
+        Object item = lookupItem(content);
 
         if (item != null) {
             setSelectedItem(item);
-        } else {
-           // comboBox.getToolkit().beep(); // Avisar que no se encontró coincidencia
+            setText(item.toString());
+            highlightCompletedText(offs + str.length());
         }
-
-        highlightCompletedText(currentText.length());
     }
 
     private void setText(String text) {
@@ -140,7 +149,7 @@ public final class AutoCompletarComboBox extends PlainDocument {
             super.remove(0, getLength());
             super.insertString(0, text, null);
         } catch (BadLocationException e) {
-            throw new RuntimeException(e.toString());
+            throw new RuntimeException(e);
         }
     }
 
@@ -157,12 +166,12 @@ public final class AutoCompletarComboBox extends PlainDocument {
 
     private Object lookupItem(String pattern) {
         Object selectedItem = model.getSelectedItem();
-        if (selectedItem != null && containsIgnoreCase(selectedItem.toString(), pattern)) {
+        if (selectedItem != null && startsWithIgnoreCase(selectedItem.toString(), pattern)) {
             return selectedItem;
         } else {
             for (int i = 0, n = model.getSize(); i < n; i++) {
                 Object currentItem = model.getElementAt(i);
-                if (currentItem != null && containsIgnoreCase(currentItem.toString(), pattern)) {
+                if (currentItem != null && startsWithIgnoreCase(currentItem.toString(), pattern)) {
                     return currentItem;
                 }
             }
@@ -170,8 +179,48 @@ public final class AutoCompletarComboBox extends PlainDocument {
         return null;
     }
 
-    private boolean containsIgnoreCase(String str1, String str2) {
-        if (str1 == null || str2 == null) return false;
-        return str1.toLowerCase().contains(str2.toLowerCase());
+    private boolean startsWithIgnoreCase(String str1, String str2) {
+        return str1.toLowerCase().startsWith(str2.toLowerCase());
+    }
+
+    private void verificarItemNoEncontrado() {
+        String text = editor.getText();
+        Object item = findExactMatch(text);
+
+        if (item == null && !mensajeMostrado) {
+            mensajeMostrado = true;
+
+            if (!editable) {
+                if (mostrarMensaje) {
+                    JOptionPane.showMessageDialog(null, "Item no encontrado");
+                }
+                comboBox.setSelectedIndex(-1); // NO selecciona nada
+            }
+        }
+    }
+
+    private Object findExactMatch(String text) {
+        for (int i = 0; i < model.getSize(); i++) {
+            Object item = model.getElementAt(i);
+            if (item != null && item.toString().equalsIgnoreCase(text)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public static boolean esItemValido(JComboBox comboBox) {
+        ComboBoxModel model = comboBox.getModel();
+        Object editorObj = comboBox.getEditor().getEditorComponent();
+        if (editorObj instanceof JTextComponent) {
+            String text = ((JTextComponent) editorObj).getText();
+            for (int i = 0; i < model.getSize(); i++) {
+                Object item = model.getElementAt(i);
+                if (item != null && item.toString().equalsIgnoreCase(text)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
